28c7fe093f34be1b65e2b5700c44c942ed0a1f4a
naelah <naelahnordin@gmail.com>
2018-10-12 18:00:00 +0900

    fix nav pills

--- resources/views/admin_pages/salons/settings.blade.php
+++ resources/views/admin_pages/salons/settings.blade.php
@@ -1,5 +1,172 @@
-<p>
 
-hello hubba hubba
-
-</p>
+<div class="row">
+  <div class="col-xl-12 order-xl-1">
+    <form>
+      <h3 class="mb-0">My account</h3>
+      <h6 class="heading-small text-muted mb-4">Store information</h6>
+      <div class="row">
+        <div class="col-lg-12">
+          <div style="height: 200px ; transform: none;" class="card-profile-image">
+            <a href="#">
+              <img src="/img/theme/ivy-salon.png" class="rounded shadow-lg">
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="form-group">
+            <label class="form-control-label" for="input-username">Display name</label>
+            <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Username" value="lucky.jesse">
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="form-group">
+            <label class="form-control-label" for="input-status">Status</label>
+            <div class="row">
+              <label style="margin-left:20px;" class="custom-toggle">
+                <input type="checkbox" checked><span class="custom-toggle-slider rounded-circle"></span> 
+              </label>
+              <h4 class="mb-0 text-primary" style="margin-left:20px;" >Active</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="form-group">
+            <label class="form-control-label" for="input-email">Email address</label>
+              <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="jesse@example.com">
+              <small id="telHelpBlock" class="form-text text-muted">
+                Reservation notification & promotions will be sent to this email.
+              </small>  
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="form-group">
+            <label class="form-control-label" for="input-status">Email Notifications</label>
+            <div class="row">
+              <label style="margin-left:20px;" class="custom-toggle">
+                <input type="checkbox" checked><span class="custom-toggle-slider rounded-circle"></span> 
+              </label>
+              <h4 class="mb-0 text-primary" style="margin-left:20px;" >Enabled</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="form-group">
+            <label class="form-control-label" for="input-first-name">Telephone Number</label>
+            <input type="tel" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="0934252432">  
+            <small id="telHelpBlock" class="form-text text-muted">
+                Reservation notification will be sent to this number. You can add multiple numbers separated by comma (Eg: 097867855, 9096785673)
+            </small>   
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="form-group">
+            <label class="form-control-label" for="input-status">Message Notifications</label>
+              <div class="row">
+                <label style="margin-left:20px;" class="custom-toggle">
+                  <input type="checkbox" checked><span class="custom-toggle-slider rounded-circle"></span> 
+                </label>
+                <h4 class="mb-0 text-primary" style="margin-left:20px;" >Enabled</h4>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-5">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="form-group">
+                <label class="form-control-label" for="input-first-name">Facebook</label>
+                <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="Lucky">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="form-group">
+                <label class="form-control-label" for="input-last-name">URL</label>
+                <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="Jesse">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-7">
+          <div class="form-group">
+            <label class="form-control-label" for="input-last-name">Days open in a week</label>
+            <div class="row">
+              <div class="col-lg-12">
+                <label class="btn btn-3 btn-outline-default btn-sm "><span class="btn-inner--text">M</span><input type="checkbox" name="chk1" id="item4" value="val1" class="hidden" autocomplete="off"></label>
+                <label class="btn btn-3 btn-outline-default btn-sm"><span class="btn-inner--text">T</span><input type="checkbox" name="chk1" id="item4" value="val1" class="hidden" autocomplete="off"></label>
+                <label class="btn btn-3 btn-outline-default btn-sm"><span class="btn-inner--text">W</span><input type="checkbox" name="chk1" id="item4" value="val1" class="hidden" autocomplete="off"></label>
+                <label class="btn btn-3 btn-outline-default btn-sm"><span class="btn-inner--text">T</span><input type="checkbox" name="chk1" id="item4" value="val1" class="hidden" autocomplete="off"></label>
+                <label class="btn btn-3 btn-outline-default btn-sm"><span class="btn-inner--text">F</span><input type="checkbox" name="chk1" id="item4" value="val1" class="hidden" autocomplete="off"></label>
+                <label class="btn btn-3 btn-outline-default btn-sm"><span class="btn-inner--text">S</span><input type="checkbox" name="chk1" id="item4" value="val1" class="hidden" autocomplete="off"></label>
+                <label class="btn btn-3 btn-outline-default btn-sm"><span class="btn-inner--text">S</span><input type="checkbox" name="chk1" id="item4" value="val1" class="hidden" autocomplete="off"></label>
+              </div> 
+            </div>	
+          </div>
+          <div class="row">
+            <div class="col-lg-6">
+              <label class="form-control-label" for="input-last-name">Opening Hours start</label>
+              <input type="time" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="10:00">
+              <small id="telHelpBlock" class="form-text text-muted">
+                Please use 24 hour format
+              </small>  
+            </div>
+            <div class="col-lg-6">
+              <label class="form-control-label" for="input-last-name">Opening Hours End</label>
+              <input type="time" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="17:00">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="form-group">
+            <label class="form-control-label" for="input-address">Address</label>
+            <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="form-group">
+            <label class="form-control-label" for="input-city">City</label>
+              <input type="text" id="input-city" class="form-control form-control-alternative" placeholder="City" value="New York">
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="form-group">
+            <label class="form-control-label" for="input-country">Country</label>
+            <input type="text" id="input-country" class="form-control form-control-alternative" placeholder="Country" value="United States">
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="form-group">
+            <label class="form-control-label" for="input-country">Postal code</label>
+            <input type="number" id="input-postal-code" class="form-control form-control-alternative" placeholder="Postal code">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="form-group">
+            <label>Store Description</label>
+            <textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
+            <small id="telHelpBlock" class="form-text text-muted">
+              This description will be displayed in Salon profile 
+            </small>            
+          </div>
+        </div>
+      </div>
+      <div class="float-right">
+        <button type="button" class="btn btn-outline-success">Preview Profile</button><button type="submit" class="btn btn-success">Save Changes</button>
+      </div>
+    </form>
+  </div>
+</div>
